7432f869cd9cd97e78a946036520c90e73b5148c
Shahid Abbas <abbaspcr3@gmail.com>
2022-07-12 00:30:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div><form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form></div>;
+  return <div>Hello</div>;
 }
 
 export default App;
